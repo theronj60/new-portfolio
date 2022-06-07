e1ee2b07fd6b30493a27df4a2ee84c5d9ed8e0da
Theron Joe <theronj@joetwebdev.io>
2022-06-07 01:14:30 -0600

    updated npm and composer. adding information to 'stack' portion of webpage. WIP.

--- resources/js/pages/main/Stack.vue
+++ resources/js/pages/main/Stack.vue
@@ -11,8 +11,25 @@
 </script>
 <template>
 	<section id="stack" class="min-h-screen flex items-center justify-center" :style="bgImg">
-		<div class="text-2xl text-white">
-			Software, Text Editors, Frameworks
+		<div class="grid grid-cols-4 space-x-4 space-y-k items-center text-2xl text-white">
+			<div class="flex flex-col items-center justify-center border-2 bg-gray-700 bg-opacity-50 p-2 rounded-xl shadow">
+				<div class="bg-black rounded h-16 w-16">
+					<!-- will hold image -->
+				</div>
+				<div>
+					Neovim
+				</div>
+				<span class="text-sm">My preferred editor of choice.</span>
+			</div>
+			<div class="flex border-2 bg-gray-700 bg-opacity-50 rounded-xl shadow">
+				Vue 2/3
+			</div>
+			<div class="flex border-2 bg-gray-700 bg-opacity-50 rounded-xl shadow">
+				Laravel
+			</div>
+			<div class="flex border-2 bg-gray-700 bg-opacity-50 rounded-xl shadow">
+				Tailwind CSS
+			</div>
 		</div>
 	</section>
 </template>
